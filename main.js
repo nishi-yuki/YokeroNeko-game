76b14d8ef3ed197f63d1df3aaaaccc42ac444a2b
Yuki Nishi <nishiyk256@gmail.com>
2017-07-25 09:02:10 +0900

    Nekoクラスを作成 add Neko calss

--- main.js
+++ main.js
@@ -12,51 +12,63 @@ var SCREEN_HEIGHT = 900;
 var GRAVITY = 1;
 var GROUND_FACE = 850;
 var CEILING = 10;
+var JUMP_POWER = -20;
 
 // MainScene クラスを定義
 phina.define('MainScene', {
-    superClass: 'CanvasScene',
+    superClass: 'DisplayScene',
     init: function(options) {
         this.superInit(options);
         // 背景色を指定
         this.backgroundColor = 'skyblue';
-
-        var neko = Sprite('neko').addChildTo(this);
+        //nekoを生成
+        var neko = Neko().addChildTo(this);
         neko.x = this.gridX.center();
         neko.y = this.gridY.center();
-        neko.width = 128;
-        neko.height = 128;
 
-        neko.xcenter = this.gridX.center();
-        neko.ycenter = this.gridY.center();
-
+        //maruを追加
+        var maru = CircleShape({
+            fill: 'gray',
+        }).addChildTo(this);
+        maru.physical.velocity.x = -10;
         // 画面タッチ時処理 
         this.onpointend = function() {
-            neko.physical.velocity.y = -20;
-        }
-
-        neko.update = function() {
-            //床で止まる処理
-            if (neko.y > GROUND_FACE + 10) {
-                this.physical.gravity.y = 0;
-                this.physical.velocity.y = this.physical.velocity.y * -0.5;
-                this.y = GROUND_FACE + 5;
+                neko.jump();
             }
-            if (neko.y < GROUND_FACE) {
-                this.physical.gravity.y = GRAVITY;
+            //maruの挙動
+        maru.update = function() {
+            if (maru.x < 10) {
+                maru.x = SCREEN_WIDTH;
+                maru.y = Math.randint(0, SCREEN_HEIGHT);
             }
-            //天井処理
-
-            if (neko.y < CEILING) {
-                if (this.physical.velocity.y < 0)
-                    this.physical.velocity.y = 0;
-            }
-
         };
-
     }
 });
-
+//nekoクラス
+phina.define('Neko', {
+    superClass: 'Sprite',
+    init: function() {
+        this.superInit('neko');
+        this.width = 128;
+        this.height = 128;
+        this.physical.gravity.y = GRAVITY;
+    },
+    jump: function() {
+        this.physical.velocity.y = JUMP_POWER;
+    },
+    update: function() {
+        //床で跳ねる
+        if (this.bottom > GROUND_FACE) {
+            this.bottom = GROUND_FACE - 5;
+            this.physical.velocity.y = this.physical.velocity.y * -0.5;
+        } //天井に当たる
+        else if (this.top < CEILING) {
+            if (this.physical.velocity.y < 0) {
+                this.physical.velocity.y = 0;
+            }
+        }
+    },
+});
 // メイン処理
 phina.main(function() {
     // アプリケーション生成
