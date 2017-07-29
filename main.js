59894de4ee18e75e1e30eb2dd5aa142b536bacfd
Yuki Nishi <nishiyk256@gmail.com>
2017-07-29 10:19:19 +0900

    Maruクラスにまとめた

--- main.js
+++ main.js
@@ -27,22 +27,15 @@ phina.define('MainScene', {
         neko.y = this.gridY.center();
 
         //maruを追加
-        var maru = CircleShape({
+        var maru = Maru({
             fill: 'gray',
-        }).addChildTo(this);
-        maru.physical.velocity.x = -10;
+        }, neko).addChildTo(this);
+
         // 画面タッチ時処理 
         this.onpointend = function() {
-                neko.jump();
-            }
-            //maruの挙動
-        maru.update = function() {
-            if (maru.x < 10) {
-                maru.x = SCREEN_WIDTH;
-                maru.y = Math.randint(0, SCREEN_HEIGHT);
-            }
+            neko.jump();
         };
-    }
+    },
 });
 //nekoクラス
 phina.define('Neko', {
@@ -68,6 +61,28 @@ phina.define('Neko', {
             }
         }
     },
+});
+//Maruクラスの定義
+phina.define('Maru', {
+    superClass: 'RectangleShape',
+    init: function(option, enemy) {
+        this.superInit(option);
+        this.physical.velocity.x = -10;
+        this.enemy = enemy;
+    },
+    update: function() {
+        if (this.right < 0) {
+            this.fill = 'yellow'
+            this.left = SCREEN_WIDTH;
+            this.y = Math.randint(CEILING, GROUND_FACE);
+            this.hit_already = false;
+        }
+        if (this.hitTestElement(this.enemy) && this.hit_already == false) {
+            this.hit_already = true;
+            this.fill = 'green';
+        }
+    },
+    hit_already: false,
 });
 // メイン処理
 phina.main(function() {
